79c57ee1c597d09a80a5c1c16166784727e5c453
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-11 18:27:04 +0700

    feat: add wrapper

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,11 @@
+export default function Wrapper({children, additionalClass}) {
+  return (
+    <div className="bg-zinc-100/95 fixed top-0 left-0 w-full h-screen overflow-hidden">
+      <div className={`max-w-[480px] mx-auto h-full shadow-md backdrop-blur-2xl ${additionalClass}`}>
+        <div className="h-full overflow-y-scroll">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
