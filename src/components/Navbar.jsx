2c9fd2278183fbbeeebe63b3c66448c057484667
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-11 20:24:20 +0700

    feat: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,48 @@
 import {FaHouse} from "react-icons/fa6";
 import {FaBell, FaBook, FaQrcode, FaUser} from "react-icons/fa";
 
-export default function Navbar() {
+export default function Navbar({whichActive}) {
   return (
     <div className="fixed bottom-0 w-full flex justify-center items-center">
       <div
-        className="bg-[#005EEC] shadow-lg p-3 mb-5 flex justify-between items-center rounded-full max-w-md w-full mx-5">
-        <div
-          className="flex flex-row items-center gap-2 bg-white text-blue-600 rounded-full box-boder p-3">
+        className="bg-[#005EEC] shadow-lg p-3 mb-5 flex justify-around items-center rounded-full max-w-md w-full mx-5">
+        <div className={`flex flex-row items-center gap-2  rounded-full box-border 
+          ${whichActive === "Home" ? 'text-blue-600 bg-white p-3' : ''}`}>
           <FaHouse/>
-          <span className="text-xs font-semibold">Home</span>
+          {whichActive === "Home" &&
+            <span className="text-xs font-semibold">Home</span>
+          }
         </div>
-
-        <div
-          className="flex flex-row items-center text-wht-600 gap-2  text-white rounded-full box-boder p-3">
+        <div className={`flex flex-row items-center gap-2 p-3 rounded-full box-border 
+          ${whichActive === "Profile" ? 'text-blue-600 bg-white ' : ''}`}>
           <FaUser/>
 
+          {whichActive === "Profile" &&
+            <span className="text-xs font-semibold">Profile</span>
+          }
         </div>
-        <div
-          className="flex flex-row items-center text-wht-600 gap-2  text-white rounded-full box-boder p-3">
+        <div className={`flex flex-row items-center gap-2 p-3 rounded-full box-border 
+          ${whichActive === "House" ? 'text-blue-600 bg-white' : ''}`}>
           <FaQrcode/>
+          {whichActive === "House" &&
+            <span className="text-xs font-semibold">Home</span>
+          }
         </div>
-        <div
-          className="flex flex-row items-center text-wht-600 gap-2  text-white rounded-full box-boder p-3">
+
+
+        <div className={`flex flex-row items-center gap-2 p-3  rounded-full box-border 
+          ${whichActive === "House" ? 'text-blue-600 bg-white ' : ''}`}>
           <FaBell/>
-
+          {whichActive === "House" &&
+            <span className="text-xs font-semibold">Home</span>
+          }
         </div>
-
-        <div
-          className="flex flex-row items-center text-wht-600 gap-2  text-white rounded-full box-boder p-3">
+        <div className={`flex flex-row items-center gap-2 p-3 rounded-full box-border 
+          ${whichActive === "House" ? 'text-blue-600 bg-white ' : ''}`}>
           <FaBook/>
+          {whichActive === "House" &&
+            <span className="text-xs font-semibold">Home</span>
+          }
         </div>
 
       </div>
