f6d31e6fe5806d12713aa8b1c98b4aa734c4b008
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-13 15:35:25 +0700

    feat: update

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,6 +1,6 @@
 export default function Wrapper({children, additionalClass}) {
   return (
-    <div className="bg-zinc-100/95 fixed top-0 left-0 w-full h-full min-h-screen  overflow-auto">
+    <div className="bg-zinc-100/95 fixed top-0 left-0 w-full h-full min-h-screen overflow-y-scroll">
       <div className={`max-w-[480px]  mx-auto h-full shadow-md backdrop-blur-2xl ${additionalClass}`}>
         <div className="h-full overflow-y-scroll ">
           {children}
