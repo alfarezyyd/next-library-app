dd606e9509d9fcfb0946855cec1854d44b6f6829
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-14 08:01:32 +0700

    feat: change active

--- src/app/admin/books/page.jsx
+++ src/app/admin/books/page.jsx
@@ -113,7 +113,7 @@ export default function Page() {
           </Table>
         }
       </div>
-      <Navbar whichActive={'Notification'}/>
+      <Navbar whichActive={'About'}/>
     </Wrapper>
   )
 }
