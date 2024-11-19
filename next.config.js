5cb713d17cf4b963fb42d615de435c225d746e99
alfarezyyd <adityaalfarezyd@gmail.com>
2024-11-19 14:23:31 +0700

    feat: upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; img-src 'self' https://storage.googleapis.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
